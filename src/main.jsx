30232f4c5fe54a31d5c3f2b4db6b6c10de2b4726
kushal <mohitsinghkpsc@gmail.com>
2023-10-18 18:44:57 +0530

    Started basic redux setup with toolkit

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,17 @@ import "./index.css";
 
 import React from "react";
 import ReactDOM from "react-dom/client";
-import {Toaster} from "react-hot-toast"
+import { Toaster } from "react-hot-toast";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
+import store from './Redux/store';
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <App />
-    <Toaster/>
-  </BrowserRouter>
+  // iska use karne se hamara store har jagah activate ho gaya hai we can use everything..
+  <Provider store = {store}>    
+    <BrowserRouter>
+      <App />
+      <Toaster />
+    </BrowserRouter>
+  </Provider>
 );
