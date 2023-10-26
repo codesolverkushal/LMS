b24c8b647a9edb42f1a013895cceccc4f035781e
kushal <mohitsinghkpsc@gmail.com>
2023-10-26 10:58:30 +0530

    aboutus page done

--- src/layouts/HomeLayout.jsx
+++ src/layouts/HomeLayout.jsx
@@ -50,7 +50,7 @@ function HomeLayout({ children }) {
                 </div>
                 <div className="drawer-side w-0">
                     <label htmlFor="my-drawer" className="drawer-overlay"></label>
-                    <ul className='menu p-4 w-48 sm:w-80 bg-base-200 text-base-content relative'>
+                    <ul className='menu p-4 h-[100%] w-48 sm:w-80 bg-base-200 text-base-content relative'>
                         <li className='w-fit absolute right-2 z-50'>
                             <button onClick={hideDrawer}>
                                 <AiFillCloseCircle size={24} className='hover:text-blue-500'/>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 
 
-import HomeLayout from "../layouts/HomeLayout";
+import HomeLayout from "../Layouts/HomeLayout";
 function Home() {
 
     return (
